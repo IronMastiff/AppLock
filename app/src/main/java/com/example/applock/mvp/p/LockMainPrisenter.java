6f1a61c3bea77b386683250e8a2dadbe41ee8eb4
Iron Mastiff <1760298283@qq.com>
2017-08-20 17:38:37 +0800

    加入LockMainPrisenter

---
+++ app/src/main/java/com/example/applock/mvp/p/LockMainPrisenter.java
@@ -0,0 +1,10 @@
+package com.example.applock.mvp.p;
+
+import com.example.applock.mvp.contract.LockMainContract;
+
+/**
+ * Created by 我的 on 2017/8/20.
+ */
+
+public class LockMainPrisenter implements LockMainContract {
+}
