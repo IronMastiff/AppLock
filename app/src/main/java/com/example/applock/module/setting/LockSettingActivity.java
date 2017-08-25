96b0361369b9a4dd654fa7256270929b780071fe
Iron Mastiff <1760298283@qq.com>
2017-08-25 14:26:12 +0800

    完成MainActivity

---
+++ app/src/main/java/com/example/applock/module/setting/LockSettingActivity.java
@@ -0,0 +1,12 @@
+package com.example.applock.module.setting;
+
+import com.example.applock.base.BaseActivity;
+
+import android.content.DialogInterface;
+
+/**
+ * Created by 我的 on 2017/8/25.
+ */
+
+public class LockSettingActivity extends BaseActivity implements DialogInterface.OnClickListener {
+}
