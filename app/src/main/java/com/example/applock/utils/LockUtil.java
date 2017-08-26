f5f22406d95b21e78092d06cd64145ef3c9240bf
Iron Mastiff <1760298283@qq.com>
2017-08-26 21:47:46 +0800

    增加LockUtil

---
+++ app/src/main/java/com/example/applock/utils/LockUtil.java
@@ -0,0 +1,17 @@
+package com.example.applock.utils;
+
+import android.content.Context;
+
+/**
+ * Created by 我的 on 2017/8/26.
+ */
+
+public class LockUtil {
+    /**
+     * 判断是否已经获取/有权查看使用的应用程序权限
+     */
+    public static boolean isStatAccessPermissionSet( Context context ){
+        
+    }
+
+}
